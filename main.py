10f7295caa004d6d855c0d09be06eceefacc7f84
Sam Weston <sam.weston@tails.com>
2022-01-03 10:31:57 +0000

    Lots more complexity

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import datetime
 
 import requests
@@ -5,14 +6,39 @@ import requests
 API_BASE = 'https://api.onepeloton.co.uk/api/'
 COOKIE = '584455b4a61846988936ee504407f2fd'
 CHALLENGE_START = datetime.datetime(2021, 11, 22, 0, 0)
+USER_DICT = {
+        '32df2a456cd94a77bbf3a6c32af6cb26': 'Nicki_Cycology',
+        '945ba7f2f9144c45b6c2c74c748f107b': 'irish_claire',
+        'f9761e7da1cd4eb8a7549fecf92598d6': 'lockandloaded',
+        'e7336ba47aca45d2a2be42ea9faf8c35': 'CruellaDeSpin',
+        '925f924297934aabba1f738bd9d89522': 'pastheicecream',
+        '560ef299756044cda5e50ccad8d5f89c': 'PhilH79',
+        'ac540877948e4a7e8149d5abd87bc6ce': 'GingerNut1',
+        '530639823f514afd98cb309d6d82e116': 'WeMoveTogether',  # 2 activites in 6
+        'a241748577e241e597d36630f97f06ea': 'CrystalMet',
+        '8b09d1313f04427a9bcdf51bb5ef0799': 'Kay_UK',
+        '6d5ad73229334f08805579790f755516': 'allycatty',
+        'e56af6663ccc4bc6b6417e3079fa074a': 'Charlie_73',  # 2 activities in 6
+        '5f92c512d89b4e9ab4d8a3c074198b88': 'louise12112',
+        'ad3f1733b146487abef1d7375873e661': 'Twinmum23',
+        '346a716b00b44069b83d67f5cdf64aad': 'Natt82',
+        '814d84035a694053a337829c108826e6': 'Julesymc22',
+        '85a87f4d37014a8596f577e2b531dc0d': 'Wupsadaisy',
+        'a84860ef1cb6412882a1d12f0bdb133b': 'JacTay',
+        'cfbe2ff87f904ecbb7f7721152c27a18': 'GinaUK',  # 2/3 week 6
+        'ab4dd8ba84fd483c8ab05bc24b722c74': 'what_katy_did',
+        '649caab816b54321b3e200b2f993921f': 's5stw',
+        'f3a75d4cc8f34ed5812cb6ca9a63b50c': 'taz520',
+        'ade268b8be814472aaa591ced1bf6941': 'careskell'
+    }
 RUN_DICT = {
     1: {
         'start': datetime.datetime(2021, 11, 22, 0, 0),
         'end': datetime.datetime(2021, 11, 29, 0, 0),
         'runs': {
-            'f28a1a8788ca4ac5a72deed1e1171b2b':  '20 min recovery run - Susie Chan - 23/10/21',
-            'ef73518d446f4c5b83c2cfc0ce951a81':  '30 min endurance run - Becs Gentry - 3/11/21',
-            'dc35da04423644f08e07a9b29b656f07':  '30 min endurance run - Matt Wilpers - 7/9/21'
+            'f28a1a8788ca4ac5a72deed1e1171b2b': '20 min recovery run - Susie Chan - 23/10/21',
+            'ef73518d446f4c5b83c2cfc0ce951a81': '30 min endurance run - Becs Gentry - 3/11/21',
+            'dc35da04423644f08e07a9b29b656f07': '30 min endurance run - Matt Wilpers - 7/9/21'
         }
     },
     2: {
@@ -20,26 +46,26 @@ RUN_DICT = {
         'end': datetime.datetime(2021, 12, 6, 0, 0),
         'runs': {
             'ff5dca33e7b647deb3c6bfc19763a5e4': '30 min endurance run - Selena Samuela - 14/3/21',
-            'a7fca7df520742b5af771c8b12b72926':  '30 min progression run - Becs Gentry - 9/7/21',
-            'b9bfcfda74c64c639d4510987f6b4368':  '45 min endurance run - Matt Wilpers - 7/6/21'
+            'a7fca7df520742b5af771c8b12b72926': '30 min progression run - Becs Gentry - 9/7/21',
+            'b9bfcfda74c64c639d4510987f6b4368': '45 min endurance run - Matt Wilpers - 7/6/21'
         }
     },
     3: {
         'start': datetime.datetime(2021, 12, 6, 0, 0),
         'end': datetime.datetime(2021, 12, 13, 0, 0),
         'runs': {
-            '79ac1f29dd7d4c30820f048831c0bf3a':  '30 min you can run: form  - Matt Wilpers - 23/4/21',
-            '9aafb3a29e2e482bac4cc46e716db77b':  '45 min NYC Marathon simulation run - Robin Arzon - 7/10/19',
-            'e4229c0132fc4c5d998693c7d044afb2':  '30 min intervals run - Olivia Amato - 16/6/21'
+            '79ac1f29dd7d4c30820f048831c0bf3a': '30 min you can run: form  - Matt Wilpers - 23/4/21',
+            '9aafb3a29e2e482bac4cc46e716db77b': '45 min NYC Marathon simulation run - Robin Arzon - 7/10/19',
+            'e4229c0132fc4c5d998693c7d044afb2': '30 min intervals run - Olivia Amato - 16/6/21'
         }
     },
     4: {
         'start': datetime.datetime(2021, 12, 13, 0, 0),
         'end': datetime.datetime(2021, 12, 20, 0, 0),
         'runs': {
-            'ed8a76e5590c469c9e9067a3dbae0d24':  '30 min recovery run - Matt Wilpers - 3/8/20',
+            'ed8a76e5590c469c9e9067a3dbae0d24': '30 min recovery run - Matt Wilpers - 3/8/20',
             '144a8ec7f56d4949a1fdd1b22c7aaee2': '30 min tempo run - Olivia Amato - 18/11/20',
-            '0bf1c3f590784cd9b9392176d9170456':  '60 min endurance run - Marcel Dinkins - 12/9/21',
+            '0bf1c3f590784cd9b9392176d9170456': '60 min endurance run - Marcel Dinkins - 12/9/21',
             '6213fb1fbd9945c2985b3efde630cc0b': '60 min endurance run - Susie Chan - 15/12/21'
         }
     },
@@ -47,28 +73,22 @@ RUN_DICT = {
         'start': datetime.datetime(2021, 12, 20, 0, 0),
         'end': datetime.datetime(2021, 12, 27, 0, 0),
         'runs': {
-            'ac56d7fa789a43c9a7321beafbd29e97':  '30 min HIIT run - Robin Arzon - 23/9/19',
-            '0be5c37b63164379a885d8c6f8873002':  '60 min NYC Marathon simulation run - Becs Gentry - 1/11/20',
-            '8db4bbcd0232457f97071a86a0f79180':  '45 min endurance run - Selena Samuela - 22/12/20',
-            '8b73ca8b875d4a0bbd6b3fc517ee59fa':  '45 min endurance run - Becs Gentry - 23/12/21'
+            'ac56d7fa789a43c9a7321beafbd29e97': '30 min HIIT run - Robin Arzon - 23/9/19',
+            '0be5c37b63164379a885d8c6f8873002': '60 min NYC Marathon simulation run - Becs Gentry - 1/11/20',
+            '8db4bbcd0232457f97071a86a0f79180': '45 min endurance run - Selena Samuela - 22/12/20',
+            '8b73ca8b875d4a0bbd6b3fc517ee59fa': '45 min endurance run - Becs Gentry - 23/12/21'
         }
     },
     6: {
         'start': datetime.datetime(2021, 12, 27, 0, 0),
         'end': datetime.datetime(2022, 1, 3, 0, 0),
         'runs': {
-            'e1cfa230fa6842cfb8bce724ab171ce3':  '30 min recovery run - Becs Gentry - 28/10/19',
-            '27339dae48084112bbc98ae246c32bea':  '30 min endurance run - Matt Wilpers - 10/8/20',
-            '549de6c8c6dc42789d9088dfafaac015':  '45 min endurance run - Matt Wilpers - 24/9/21'
+            'e1cfa230fa6842cfb8bce724ab171ce3': '30 min recovery run - Becs Gentry - 28/10/19',
+            '27339dae48084112bbc98ae246c32bea': '30 min endurance run - Matt Wilpers - 10/8/20',
+            '549de6c8c6dc42789d9088dfafaac015': '45 min endurance run - Matt Wilpers - 24/9/21'
         }
     }
 }
-
-
-def get_users():
-    return {
-        '32df2a456cd94a77bbf3a6c32af6cb26': 'User name 1'
-    }
 
 
 def get_user_running_workouts(user_id):
@@ -98,25 +118,59 @@ def get_total_distance(user_id, user_name):
     user_activities = get_user_running_workouts(user_id)
     distance_total = 0
     pr_counter = 0
+    total_run_counter = 0
+    run_week_counter_dict = {
+        1: {},
+        2: {},
+        3: {},
+        4: {},
+        5: {},
+        6: {}
+    }
     for activity in user_activities:
         if activity['metrics_type'] != 'running' and activity['status'] != 'COMPLETE':
             continue
         activity_class_id = activity['ride']['id']
         activity_time = datetime.datetime.utcfromtimestamp(activity['start_time'])
-        pr = activity['is_total_work_personal_record']
 
         for run_week, run_week_dict in RUN_DICT.items():
             if run_week_dict['start'] < activity_time < run_week_dict['end']:
                 if activity_class_id in run_week_dict['runs'].keys():
                     distance = get_distance_for_activity(activity)
 
-                    print(f"Found matching activity for {user_name} - Week {run_week} - {run_week_dict['runs'][activity_class_id]} - {distance}Mi")
-                    if pr:
+                    print(
+                        f"Found matching activity for {user_name} - Week {run_week} - {run_week_dict['runs'][activity_class_id]} - {distance}Mi")
+
+                    # Deal with people doing the same run twice
+                    if activity_class_id in run_week_counter_dict[run_week]:
+                        print(f"Found multiple attempts for {run_week_dict['runs'][activity_class_id]}")
+                        if run_week_counter_dict[run_week][activity_class_id] > distance:
+                            print(f"This one is shorter, ignoring")
+                            break
+                        else:
+                            print(f"This one is longer, overwriting")
+                            distance_total -= run_week_counter_dict[run_week][activity_class_id]
+                            total_run_counter -= 1
+
+                    if is_activity_pr(activity):
                         print(f"PR ALERT!!!")
                         pr_counter += 1
+
                     distance_total += distance
+                    run_week_counter_dict[run_week][activity_class_id] = distance
+                    total_run_counter += 1
                 break
-    return distance_total, pr_counter
+
+    completed_challenge = True
+    for run_week, run_week_activities in run_week_counter_dict.items():
+        if len(run_week_activities) < 3:
+            print(f'WARNING: User {user_name} did {len(run_week_activities)} in week {run_week}')
+            completed_challenge = False
+        elif len(run_week_activities) > 3:
+            run_week_activities.keys()
+            print(f'WARNING: User {user_name} did {len(run_week_activities)} in week {run_week}')
+
+    return distance_total, pr_counter, completed_challenge, total_run_counter
 
 
 def get_distance_for_activity(activity):
@@ -129,8 +183,43 @@ def get_distance_for_activity(activity):
         if distance_response_summary['slug'] == 'distance':
             return distance_response_summary['value']
 
+    return 0
+
+
+def is_activity_pr(activity):
+    workout_response = requests.get(
+        f"{API_BASE}workout/{activity['id']}",
+        cookies={'peloton_session_id': COOKIE}
+    )
+    workout_response_object = workout_response.json()
+    if 'achievement_templates' in workout_response_object:
+        for achievement in workout_response_object['achievement_templates']:
+            if achievement['slug'] == 'best_output':
+                return True
+
+    return False
+
 
 if __name__ == '__main__':
-    for user_id, user_name in get_users().items():
-        user_distance, user_prs = get_total_distance(user_id, user_name)
-        print(f"Total distance for {user_name} is {round(user_distance,2)}Mi - They got {user_prs} PRs")
+    total_challenge_distance = 0
+    total_challenge_runs = 0
+    with open('savannah_run.csv', 'w', newline='') as csv_file:
+        fieldnames = ['Username', 'Distance (Miles)', 'PRs', 'Completed Challenge']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for user_id, user_name in USER_DICT.items():
+            user_distance, user_prs, completed_challenge, total_runs = get_total_distance(user_id, user_name)
+            print(f"Total distance for {user_name} is {round(user_distance, 2)}Mi - They got {user_prs} PRs")
+            writer.writerow(
+                {
+                    'Username': user_name,
+                    'Distance (Miles)': round(user_distance, 2),
+                    'PRs': user_prs,
+                    'Completed Challenge': completed_challenge
+                }
+            )
+            print(f"User {user_name} did not complete the challenge but distance was {round(user_distance, 2)}Mi")
+            total_challenge_distance += user_distance
+            total_challenge_runs += total_runs
+    print(f"Total Challenge Distance was {total_challenge_distance}Mi")
+    print(f"Total Challenge Runs was {total_challenge_runs}")
